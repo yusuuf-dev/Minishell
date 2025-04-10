97b0303665c16a810a9e2ab4612af2f4937c2fb1
Ayoub Soufiani <asoufian@c2r4p1.1337.ma>
2025-04-10 09:34:54 +0100

    cd, pwd, echo

--- test_readline.c
+++ test_readline.c
@@ -6,7 +6,7 @@
 /*   By: asoufian <asoufian@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:49:41 by asoufian          #+#    #+#             */
-/*   Updated: 2025/04/09 09:29:20 by asoufian         ###   ########.fr       */
+/*   Updated: 2025/04/10 09:34:19 by asoufian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <readline/history.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 typedef struct history {
 	char *s;
@@ -89,6 +91,108 @@ char	*ft_strdup(char *s)
 	p[i] = 0;
 	return (p);
 }
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	t;
+	char	*p;
+
+	i = 0;
+	t = 0;
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	p = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while(s2[t])
+	{
+		p[t + i] = s2[t];
+		t++;
+	}
+	p[t + i] = 0;
+	free(s1);
+	return (p);
+}
+
+char	*ft_strldup(char *s, size_t	n)
+{
+	size_t	i;
+	size_t	len;
+	char	*p;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	if (n <= len)
+		p = malloc(n + 1);
+	else
+		p = malloc(len + 1);
+	while (s[i] && i < n)
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = 0;
+	return (p);
+}
+
+
+char	*ft_remove_isspace(char *s)
+{
+	size_t	i;
+	size_t	t;
+	size_t	count;
+	char	*p;
+
+	i = 0;
+	t = 0;
+	count = 0;
+	if (!s)
+		return NULL;
+	while ((s[t] >= 9 && s[t] <= 13) || s[t] == ' ')
+		t++;
+	while (s[i + t])
+	{
+		if ((s[i + t] >= 9 && s[i + t] <= 13) || s[i + t] == ' ')
+		{
+			while ((s[i + t] >= 9 && s[i + t] <= 13) || s[i + t] == ' ')
+				t++;
+			if (s[i + t])
+				count++;
+		}
+		i++;
+	}
+	p = malloc(i + count + 1);
+	i = t = 0;
+	while ((s[t] >= 9 && s[t] <= 13) || s[t] == ' ')
+		t++;
+	while (s[t])
+	{
+	//	p[i] = s[t];
+		if ((s[t] >= 9 && s[t] <= 13) || s[t] == ' ')
+		{
+			while ((s[t] >= 9 && s[t] <= 13) || s[t] == ' ')
+				t++;
+			if (s[t])
+				p[i] = ' ';
+		}
+		else
+		{
+			p[i] = s[t];
+			t++;
+		}
+		i++;
+	}
+	p[i] = 0;
+	return (p);
+}
 void	signal_handler(int signum)
 {
 //	printf("%d\n", rl_end);
@@ -107,14 +211,121 @@ void	signal_handler(int signum)
 //	int	end_prompt = rl_end();
 //	return (0);
 }
-int	main(void)
+int	ft_echo(char *p1)
+{
+	char	*s;
+
+	s = ft_strchr(p1, 'o');
+		//	arg[0] = "echo";
+		//	arg[1] = s + 2;
+	if (!s[1])
+	{
+		printf("\n");
+			//	execve("/usr/bin/echo", arg, NULL);
+			//check for error of execve
+	}
+	else if (s[1] == ' ')
+	{
+		s = ft_remove_isspace(s + 2);
+	//	execve("/usr/bin/echo", arg, NULL);
+		//check for error of execve
+		if (!(ft_strcmp("-n ", s, 3)))
+			printf("%s", (s + 3));
+		else
+			printf("%s\n", (s));
+		free(s);
+		//	return (0);
+//			exit(0);
+	}
+	else
+	{
+		printf("%s: command not found\n", p1);
+//		exit(127);
+//		return (127);
+	}
+	return (0);
+}
+int	ft_pwd(void)
+{
+	char	*p2;
+
+	p2 = NULL;
+	p2 = getcwd(p2, 4100);
+	printf("%s\n", p2);
+	free(p2);
+	return (0);
+}
+
+int	ft_cd(char	*p1)
+{
+	char	*p = NULL;
+	char	*p2 = NULL;
+	
+	p = getcwd(p, 4100);
+	printf("prev: %s\n", p);
+	free(p);
+	p = NULL;
+	if (chdir(ft_strchr(p1, ' ') + 1))
+	{
+		printf("error while changing dir\n");
+		exit (-1);
+	}
+	p = getcwd(p, 4100);
+	printf("curr: %s\n", p);
+}
+
+int	ft_export(char *p1, char ***envp)
+{
+	char **temp;
+	char	*s;
+	char	*new;
+	int	size;
+	size_t	i;
+
+	i = 0;
+	size = 0;
+	temp = *envp;
+	if (*(ft_strchr(p1, 't') + 1) != ' ')
+	{
+		printf("Command not found\n");
+		return (-1);
+	}
+	s = ft_strchr(p1, ' ') + 1;
+	while (s[size] && s[size] != '=')
+		size++;
+	// if the exported variable exist in the environement file
+	while (temp[i])
+	{
+		if (!ft_strcmp(temp[i], s, size))
+		{
+			s = ft_strchr(s, '=');
+			new = ft_strldup(temp[i], ft_strchr(temp[i], '=') - temp[i]);
+//			new = ft_strjoin(s, "=");
+			new = ft_strjoin(new, s);
+		//	free(temp[i]); the environement is not stored in the heap :)
+			temp[i] = new;
+			return (0);
+		}
+		i++;
+	}
+	
+}
+int	main(int argc, char **argv, char **envp)
 {
 	struct sigaction C_d;
 	struct sigaction C_c;
 	int	end_prompt;
 	char	*p1;
-	char	*p2;
+	char	*p2 = NULL;
 	char	*s;
+	char	**arg;
+	int	child_pid = 0;
+	int	child_info;
+	int	status;
+	arg = malloc(sizeof(char*) * 3);
+	arg[0] = NULL;
+	arg[1] = NULL;
+	arg[2] = NULL;
 	t_cmd_history his;
 	his.next = NULL;
 	his.s = NULL;
@@ -127,59 +338,75 @@ int	main(void)
 	C_c.sa_flags = 0;
 	C_d.sa_handler = SIG_IGN;
 	C_c.sa_handler = signal_handler;
+	char *cwd = NULL;
+	cwd = getcwd(cwd, 4100);
+	printf("cwd: %s\n", cwd);
+	cwd = ft_strjoin(cwd, ": ");
+	printf("%s\n", getenv("SHELL"));
+	int i = 0;
+	while (envp[i] != NULL)
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
 	while (1)
 	{
-		//printf("%p\n", his.next);
 		sigaction(SIGQUIT, &C_d, NULL);
 		sigaction(SIGINT, &C_c, NULL);
 	// check sigaction for error;
-//	char *p = readline("prompt: ");
 //	check for readline error;
 	// try this:
 //	char *first = readline("prompt: ");
-//	sleep(3);
 //		add_history("friendly");
-	/*	if (his.s)
-		{
-			temp = &his;
-			while (temp)
-			{
-				add_history(temp->s);
-				temp = temp->next;
-			}
-		}*/
-		p1 = readline("prompt: ");
+		p1 = readline(cwd);
+		p1 = ft_remove_isspace(p1);
 		if (p1 && p1[0] == 0)
 		{
 			free(p1);
 			p1 = NULL;
 		//	p1 = readline("re_propmt: ");
 		}
+	//	if (p1)
 		else if (!p1)
 		{
 			printf("exit\n");
-			free_history(&his);
+	//		free_history(&his);
 			free(p1);
-			exit(1);
+			exit(0);
+		}
+		// need to handle the case where there are spaces before the program name
+		else if (!ft_strcmp("pwd", p1, 3)) //&& p1[3] == 0)
+		{
+			status = ft_pwd();
 		}
 		else if (!ft_strcmp("echo", p1, 4))
 		{
-			s = ft_strchr(p1, 'o');
-			if (!s[1])
-				printf("\n");
-			else if (s[1] == ' ')
-				printf("%s\n", &s[2]);
-			else
-				printf("%s: command not found\n", p1);
+			status = ft_echo(p1);
+		}
+		else if (!ft_strcmp("cd", p1, 2))
+		{
+			status = ft_cd(p1);
+		}
+		else if (!ft_strcmp("export", p1, 6))
+		{
+			status = ft_export(p1, &envp);	
+			i = 0;
+			while (envp[i] != NULL)
+			{
+				printf("%s\n", envp[i]);
+				i++;
+			}
 		}
 		else
 			printf("%s: command not found\n", p1);
+			//set the variable '?' to 127
 //		if (p1)
 //			save_to_history(&his, p1);
 		if (p1 && p1[0] != 0)
 			add_history(p1);
 		free(p1);
 	}
+	free(cwd);
 }
 void	free_history(t_cmd_history *his)
 {
