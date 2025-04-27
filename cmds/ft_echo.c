e6595cb4e2e97c56fd0ab4cdf64d4cfdc54dc38e
yusuuf-dev <lucas2030garcia@gmail.com>
2025-04-27 16:06:46 +0100

    fix echo function

--- cmds/ft_echo.c
+++ cmds/ft_echo.c
@@ -12,40 +12,6 @@
 
 #include "../minishell.h"
 
-static int  found_q(char *s, char q)
-{
-    size_t  i;
-    int     found;
-
-    i = 0;
-    found = 0;
-    while (s[i])
-    {
-        if (s[i] == q)
-            found++;
-        i++;
-    }
-    if (found % 2 != 0)
-        return(0);
-    return(1);
-}
-
-
-void    print_rm_q(char *str,char q)
-{
-    size_t  i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] != q)
-            write(1, &str[i], 1);
-        i++;
-    }
-}
-
-
-
 int     ft_echo(int argc, char **p, char **envp)
 {
 	size_t	i = 1;
