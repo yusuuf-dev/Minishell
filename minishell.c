09513fa54ae3199066328eeaf7aefa99d40d777e
yusuuf-dev <lucas2030garcia@gmail.com>
2025-05-26 16:48:14 +0100

    use ac and av in main to protect input

--- minishell.c
+++ minishell.c
@@ -1,15 +1,15 @@
 #include "minishell.h"
 
-static int     exit_minishell(char **envp, char *p, int status, char *msg);
+static int exit_minishell(char **envp, char *p, int status, char *msg);
 
-//volatile sig_atomic_t child_exists = 0;
+// volatile sig_atomic_t child_exists = 0;
 volatile sig_atomic_t f_sig = 0;
 
-void    signal_handler(int signum)
+void signal_handler(int signum)
 {
-	(void)signum;
-  //  status = 130;
-  //  write(1, "here\n", 5);
+    (void)signum;
+    //  status = 130;
+    //  write(1, "here\n", 5);
     if (f_sig != 2)
     {
         rl_replace_line("", 0);
@@ -49,54 +49,57 @@ static int reset_std_in_out_err(int fd0, int fd1, int fd2)
     if (dup2(fd1, 1) == -1)
         return (perror(""), 1);
     if (dup2(fd2, 2) == -1)
-        return (perror(""), 1);       
+        return (perror(""), 1);
     return (0);
 }
 int main(int ac, char **av, char **envp)
 {
-	char *p = NULL;
+    char *p = NULL;
     char **segments = NULL;
-	struct sigaction C_slash;
+    struct sigaction C_slash;
     struct sigaction C_c;
-    
-	int	s_exit = 0;
+
+    int s_exit = 0;
     unsigned char status = 0;
     int is_a_pipe = 0;
     int fd0, fd1, fd2;
     int i;
-//    t_data *data;
+    //    t_data *data;
 
-	(void)av; // maybe we can remove these two from the argument since we don't use them, the problem is wether the envp will work or not;
-	(void)ac; //
-	sigemptyset(&(C_slash.sa_mask));
+    if (ac != 1 || av[1])
+        return(ft_putstr("minishell doesn't require argements\n",2),1);
+
+    sigemptyset(&(C_slash.sa_mask));
     sigemptyset(&(C_c.sa_mask));
     C_slash.sa_flags = SA_RESTART;
     C_c.sa_flags = 0;
     C_slash.sa_handler = SIG_IGN;
-   // C_slash.sa_handler = ignoree;
+    // C_slash.sa_handler = ignoree;
     C_c.sa_handler = signal_handler;
-	envp = ft_duplicate(envp, 0);
+    envp = ft_duplicate(envp, 0);
     if (assign_std_in_out_err(&fd0, &fd1, &fd2))
-        {return (free_all(envp), 1);}
-    // data = ft_setup(envp);
-	while (1 && !s_exit && !is_a_pipe)
     {
-		sigaction(SIGQUIT, &C_slash, NULL);
+        return (free_all(envp), 1);
+    }
+    // data = ft_setup(envp);
+    while (1 && !s_exit && !is_a_pipe)
+    {
+        sigaction(SIGQUIT, &C_slash, NULL);
         sigaction(SIGINT, &C_c, NULL);
         if (f_sig)
         {
             status = 130;
             f_sig = 0;
         }
-		p = readline("minishell : ");
-		if (!p)
-		{
-			printf("exit\n");
-			free_all(envp);
-			free(p);
+        p = readline("minishell : ");
+        if (!p)
+        {
+            printf("exit\n");
+            free_all(envp);
+            free(p);
             rl_clear_history();
-			return (status);
-		}
+            return (status);
+        }
         // config_rdline(&p,data);
         if (!ft_isspace_to_space(&p) && p[0])
         {
@@ -104,16 +107,16 @@ int main(int ac, char **av, char **envp)
             i = found_pipe(p);
             if (i == 1)
             {
-                segments = c_split(p, '|',envp,&status);
+                segments = c_split(p, '|', envp, &status);
                 if (!segments)
-                    return(exit_minishell(envp, p, 1, "failed malloc\n"));//protect malloc
+                    return (exit_minishell(envp, p, 1, "failed malloc\n")); // protect malloc
                 free(p);
                 if (ft_pipes(segments, &p, &status, &is_a_pipe))
                     return (errno);
             }
             else if (i == -1)
-                return(exit_minishell(envp, p, 1, "failed malloc\n"));//protect malloc
-            if (p)  // not great, this is done for when the piping is done so that the program wouldn't check for cmds;
+                return (exit_minishell(envp, p, 1, "failed malloc\n")); // protect malloc
+            if (p)                                                      // not great, this is done for when the piping is done so that the program wouldn't check for cmds;
                 envp = parsing(&p, envp, &s_exit, &status, is_a_pipe);
             if (reset_std_in_out_err(fd0, fd1, fd2))
                 return (free_all(envp), 1);
@@ -122,20 +125,19 @@ int main(int ac, char **av, char **envp)
     }
     rl_clear_history();
     free_all(envp);
-    //return (0);
-    return(status);
+    // return (0);
+    return (status);
 }
 
+//
 
-// 
-
-static int     exit_minishell(char **envp, char *p, int status, char *msg)
+static int exit_minishell(char **envp, char *p, int status, char *msg)
 {
-     free_all(envp);
-     free(p);
-     if (msg)
-         printf("%s",msg);
-     exit(status);
+    free_all(envp);
+    free(p);
+    if (msg)
+        printf("%s", msg);
+    exit(status);
 }
 
 // static void    signal_handler(int signum)
@@ -207,14 +209,14 @@ static int     exit_minishell(char **envp, char *p, int status, char *msg)
 //                 i = 0;
 //                 segments = c_split(p, '|',envp);
 //                 if (!segments)
-//                     return(exit_minishell(envp,p,1,"failed malloc\n"));//protect malloc                    
+//                     return(exit_minishell(envp,p,1,"failed malloc\n"));//protect malloc
 //                 while(segments[i])
 //                 {
 //                     envp = parsing(&segments[i], envp, &s_exit);
-//                     i++;    
+//                     i++;
 //                 }
 //                 free_all(segments);
-//                 add_history(p);   
+//                 add_history(p);
 //             }
 //             else if (i == 0)
 //             {
@@ -226,5 +228,5 @@ static int     exit_minishell(char **envp, char *p, int status, char *msg)
 //         }
 //         free(p);
 //     }
-//    	exit(s_exit); 
+//    	exit(s_exit);
 // }
