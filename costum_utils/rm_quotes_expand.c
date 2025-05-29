b5ce8476dd465fb5cc38f7229a3b0e4a3af64481
yusuuf-dev <lucas2030garcia@gmail.com>
2025-05-29 17:02:35 +0100

    fix expand var dir on function: rm_quotes_expand.c

--- costum_utils/rm_quotes_expand.c
+++ costum_utils/rm_quotes_expand.c
@@ -46,6 +46,8 @@ char    *rm_quotes_expand(char *str, char **envp, unsigned char *status)
 				ptr = ft_strjoinf(ptr,var);
 			i += ft_strlen(var);
 		}
+		else if (!q && str[i] == '$' && (str[i + 1] == '\'' || str[i + 1] == '\"'))
+			i++;
 		else if (str[i] == '$' && q != '\'' && str[i + 1] == '?')
 		{
 			var = ft_getenv("?",envp,status);
