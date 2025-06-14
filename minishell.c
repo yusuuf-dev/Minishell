008c84e77208a87ba247b9f99850f551ab30aec8
youssefElyoussofi <gomezjack215@gmail.com>
2025-06-14 16:56:16 +0100

    complete merge branches

--- minishell.c
+++ minishell.c
@@ -1,311 +1,207 @@
 #include "minishell.h"
 
-/*// char	*ft_getenv(char *s, char **envp)
-// {
-// 	size_t	i = 0;
-// 	size_t	len = 0;
-
-// 	if (!s || !(s[0]))
-// 		return (NULL);
-// 	len = ft_strlen(s);
-// 	while (envp[i])
-// 	{
-// 		if (!ft_strncmp(envp[i], s, len) && envp[i][len] == '=')
-// 			return (ft_strchr(envp[i], '=') + 1);
-// 		i++;
-// 	}
-// 	return (NULL);
-// }*/
-
-int			execute_command(char *path, t_data *data);
-static int	ft_built_in_cmd(t_data *data);
-
 extern volatile sig_atomic_t f_sig;
+static int     exit_minishell(char **envp, char *p, int status, char *msg);
 
-/*static void		ft_space(char *s)
+//volatile sig_atomic_t child_exists = 0;
+
+static int assign_std_in_out_err(t_data *data)
 {
-	size_t	i = 0;
-	int		f_s = 0;
-	int		f_d = 0;
+    data->fd0 = dup(0);
+    if (data->fd0 == -1)
+        return (perror(""), 1);
+    data->fd1 = dup(1);
+    if (data->fd1 == -1)
+        return (perror(""), 1);
+    data->fd2 = dup(2);
+    if (data->fd2 == -1)
+        return (perror(""), 1);
+    return (0);
+}
 
-	while(s[i])
-	{
-		if(s[i] == '\'' && !f_d)
-			f_s = !f_s;
-		if(s[i] == '\"' && !f_s)
-			f_d = !f_d;
-		if(!f_d && !f_s && s[i] >= 9 && s[i] <= 13)
-			s[i] = ' ';
-		i++;
-	}
-}*/
-
-static int     c_strncmp(const char *s1, const char *s2) // there's another copy of this in ft_export
+static int reset_std_in_out_err(t_data *data)
 {
-    size_t  i;
+    if (dup2(data->fd0, 0) == -1)
+        return (perror(""), 1);
+    if (dup2(data->fd1, 1) == -1)
+        return (perror(""), 1);
+    if (dup2(data->fd2, 2) == -1)
+        return (perror(""), 1);       
+    return (0);
+}
 
-    i = 0;
-    while(s1[i] && s1[i] != '=')
+int main(int ac, char **av, char **envp)
+{
+    int i;
+    t_data data;
+
+	(void)av; // maybe we can remove these two from the argument since we don't use them, the problem is wether the envp will work or not;
+	(void)ac; //
+  //  __environ;
+    ft_setup(&data, envp);
+    if (assign_std_in_out_err(&data))
+        {return (free_all(envp), errno);}
+	while (!(data.exit) && !(data.is_a_pipe))
     {
-        if (s1[i] - s2[i])
-            return (s1[i] - s2[i]);
-        i++;
+		sigaction(SIGQUIT, &(data.C_slash), NULL);
+        sigaction(SIGINT, &(data.C_c), NULL);
+        if (isatty(STDIN_FILENO))
+		    data.p_rdl = readline("minishell : ");
+	    else
+	    {
+		    char *line;
+		    line = get_next_line(STDIN_FILENO);
+            if (line && line[ft_strlen(line) - 1] == '\n')
+                line[ft_strlen(line) - 1] = 0;
+		    data.p_rdl = line;
+	    }
+        if (f_sig)
+        {
+            data.status = 130;
+            f_sig = 0; // could this cause a data race ? , what if we sent SIGINT just as the proram was about to change the f_sig value ? I'm guessing it should work 
+        }               // fine since I used an atomic var 
+		if (!data.p_rdl) // C^d
+        {
+            if (isatty(STDIN_FILENO))
+                ft_putstr("exit\n", 1);
+			return (free_all(data.envp), free(data.p_rdl), rl_clear_history(), free_heredoc(&data, 1), data.status);
+        }
+        if (data.p_rdl[0])
+            add_history(data.p_rdl);
+        if (data.p_rdl[0] && check_syntax(data.p_rdl))
+            data.status = 2;
+        else if (data.p_rdl[0] && here_doc_fork(&(data.p_rdl), &(data.status), &data))
+            return (errno);
+        else if (data.p_rdl && data.p_rdl[0])
+        {
+       //     add_history(data.p_rdl);
+            i = found_pipe(data.p_rdl);
+            if (i == 1)
+            {
+                data.segments = c_split(data.p_rdl, '|', data.envp, &(data.status));
+                if (!data.segments)
+                    return(exit_minishell(data.envp, data.p_rdl, 1, "failed malloc\n"));//protect malloc
+                if (ft_pipes(&data))
+                    return (errno);
+            }
+            else if (i == -1)
+                return(exit_minishell(data.envp, data.p_rdl, 1, "failed malloc\n"));//protect malloc
+            if (data.p_rdl)  // not great, this is done for when the piping is done so that the program wouldn't check for cmds;
+                data.envp = parsing(&data);
+            if (reset_std_in_out_err(&data)) // remember to close fd{0,1,2} 
+                return (free_all(data.envp), 1);
+            /*
+            int fd_tty;
+            errno = 0;
+            fd_tty = open("/dev/tty", O_RDWR);
+            if (fd_tty == -1)
+                perror("open error ");
+            ft_putstr("something to do\n", fd_tty);
+            printf("ttyslot ret: %d\n", ttyslot());
+            printf("ttyname: %s\n", ttyname(fd_tty));
+            (void)fd_tty;*/
+        }
+        free(data.p_rdl);
     }
-	if (!(s2[i]))
-		return (0);
-    return (s1[i] - s2[i]);
+    return(rl_clear_history(), free_all(data.envp), free_heredoc(&data, 0), data.status);
 }
 
-static void ft_strcpy(char *dest, char *src)
+static int     exit_minishell(char **envp, char *p, int status, char *msg)
 {
-	size_t	i = 0;
-
-	while(src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
+     free_all(envp);
+     free(p);
+     if (msg)
+        printf("%s",msg);
+     exit(status);
 }
 
-static char	*ft_itoa(int n)
-{
-	char	*s = ft_calloc(17);
-	int		i = 15;
+// static void    signal_handler(int signum)
+// {
+// 	(void)signum;
+//     rl_replace_line("", 0);
+//     write(1, "\n", 1);
+//     rl_on_new_line();
+//     rl_redisplay();
+// }
+// static int    set_default_fd(char *term_path)
+// {
+//     size_t  i = 0;
+//     int     temp_fd = 0;
 
-	if (!n)
-		return (s[0] = '0', s);
-	while (n)
-	{
-		s[i] = (n % 10) + '0';
-		i--;
-		n = n / 10;
-	}
-	ft_strcpy(s, (s + i + 1));
-	return (s);
-}
-char	*ft_getenv(char *s, char **envp, unsigned char *status)
-{
-	size_t	i = 0;
-	// size_t	size = 0;
+//     while (i < 3)
+//     {
+//         if (!isatty(i))
+//         {
+//             temp_fd = open(term_path, O_RDWR);
+//             if (temp_fd < 0)
+//                 return (perror("minishell: setting fd"), errno);
+//             temp_fd = dup2(temp_fd, i);
+//             if (temp_fd < 0)
+//                 return (perror("minishell: setting fd"), errno);
+//         }
+//         i++;
+//     }
+//     return (0);
+// }
 
-	// size = ft_strlen(s);
-	while (envp[i])
-	{
-		if (!c_strncmp(envp[i], s))
-		{
-			return (ft_strchr(envp[i], '=') + 1);
-		}
-		if (s[0] == '?' && !s[1])
-			return (i = *status, ft_itoa(i));
-		i++;
-	}
-	return (NULL);
-}
+// int main(int ac, char **av, char **envp)
+// {
+//     char    **segments;
+// 	char *p = NULL;
+//     char    *term = NULL;
+// 	struct sigaction C_slash;
+//     struct sigaction C_c;
+// 	int	s_exit = 0;
+//     int i = 0;
 
-static int	ft_execute_cmd(t_data *data)
-{ // I need to add the status here too, so I can get the status of I run a local program (not in the PATH)
-    int     pid = 0;
-	int		child_info = 0;
-
-	if (!(data->is_a_pipe))
-    	pid = fork();
-	if (pid == -1)
-	{
-		perror("");
-		exit (-1);
-	}
-    if (pid == 0 || data->is_a_pipe)
-	{
-		signal(SIGQUIT, SIG_DFL);
-		execve(data->rdl_args[0], data->rdl_args, data->envp);
-		perror("");
-		// free allocated memory
-		exit (errno);
-	}
-    else if (pid > 0)
-	{
-		sigaction(SIGINT, &(data->C_c_alt), NULL);
-/*		if (sigaction(SIGINT, &(data->C_c_alt), NULL) == -1)
-			return (perror(""), errno);*/
-
-        waitpid(pid, &child_info, 0);
-		if (sigaction(SIGINT, &(data->C_c), NULL) == -1)
-			return (perror(""), errno);
-		if (f_sig == 2 && kill(0, SIGINT))
-			return (perror(""), errno);
-	}
-	if (WIFEXITED(child_info))
-		data->status = WEXITSTATUS(child_info);
-	else if (WIFSIGNALED(child_info))
-		data->status =  ((child_info & 127) + 128);
-    return (200);
-}
-
-static  int executable(t_data *data)
-{
-	int is_a_file = 0;
-
-	if (!data->rdl_args[0])
-		return (1);
-    if (!(ft_strchr(data->rdl_args[0], '/')))
-    {    return (0);}
-	is_a_file = open(data->rdl_args[0], O_DIRECTORY); // check for errno in case open fails for some reason, and return error ?
-	if (is_a_file != -1)
-		{return (data->status = 126, close(is_a_file), ft_putstr("minishell: ", 2), ft_putstr(data->rdl_args[0], 2), ft_putstr(": Is a directory\n", 2), 1);} // need to set the status to 126;
-    if (access(data->rdl_args[0],F_OK) == -1)
-    {
-        perror("minishell");
-        return(data->status = 127, 127);
-    }
-    if (access(data->rdl_args[0],X_OK) == 0)
-        return (ft_execute_cmd(data));
-    else
-        perror("minishell");
-    return(data->status = 126, 1);
-}
-char	**parsing(t_data *data)
-{
-    char	*env;
-    char	*path;
-	char 	*msg = NULL;
-    int		i = 0;
-	int		is_a_file = 0;
-
-  //  if (found_q(data->p_rdl) == -1) // check if the quotes are closed;
-   //     {return (ft_putstr("Error unclosed quotes\n", 2), data->envp);}
-	//ft_space(data->p_rdl);
-	//if(parse_redirection(&(data->p_rdl), &(data->status), data->envp, data)) // this also removes spaces;
-	if(parse_redirection(&(data->p_rdl), data))
-	 	return (data->envp);
-	env = ft_getenv("PATH", data->envp, &(data->status));
-	if (env)
-		data->env_paths = ft_split(env, ':');
-	else
-		data->env_paths = NULL; // In case we unset the PATH later, the pointer will be pointing to a non-valid memory (dangling pointer)
-    data->rdl_args = c_split(data->p_rdl,' ', data->envp, &(data->status));
-	
-	if (executable(data)) // next 
-		return (free_all(data->rdl_args), free_all(data->env_paths), data->envp);
-	if (ft_built_in_cmd(data))
-		(void)data->p_rdl;
-	else
-	{	
-		while (env && data->env_paths[i])
-		{
-			path = ft_strjoinf(ft_strjoin(data->env_paths[i], "/"),data->rdl_args[0]);
-			if (!access(path, F_OK))
-			{
-				if (!access(path, X_OK))
-				{
-					is_a_file = open(path, O_DIRECTORY);
-					if (is_a_file == -1) // check for errno
-						return (data->status = execute_command(path, data), free(path), free_all(data->rdl_args), free_all(data->env_paths), data->envp);
-					close(is_a_file);
-				}
-				else
-				{
-					if (!msg)
-					{
-						msg = ft_strjoin("minishell: ", path);
-						msg = ft_strjoinf(msg, ": Permission denied");
-						msg = ft_strjoinf(msg, "\n");
-					}
-					data->status = 126;
-				}
-			}
-			free(path);
-			i++;
-		}
-		if (data->status != 126)
-		{
-			ft_putstr(data->rdl_args[0], 2);
-			ft_putstr(": command not found\n", 2);
-			data->status = 127;
-		}
-		else
-		{
-			ft_putstr(msg, 2);
-			free(msg);
-		}
-	}
-	return (free_all(data->rdl_args), free_all(data->env_paths), data->envp);
-}
-
-static int	ft_built_in_cmd(t_data *data)
-{
-	char  **cmds = NULL;
-	int   i;
-
-	cmds = ft_split("pwd,cd,export,echo,env,unset,exit", ',');
-  	i = 0;
-	while (i < 10 && cmds[i])
-	{
-        if (ft_strcmp(cmds[i],data->rdl_args[0]))
-			    i += 9;
-        i++;
-	}
-	if (i == 10)
-		data->status = ft_pwd(data->rdl_args, data->envp);
-	else if (i == 11)
-		data->status = ft_cd(data->rdl_args, &(data->envp));
-	else if (i == 12)
-		data->envp = ft_export(data->rdl_args, data->envp, &(data->status));
-	else if (i == 13)
-		data->status = ft_echo(data->rdl_args);
-	else if (i == 14)
-		data->status = ft_env(data->rdl_args, data->envp, data->env_paths);
-	else if (i == 15)
-		data->envp = ft_unset(data->rdl_args, data->envp, &(data->status));
-	else if (i == 16)
-		data->status = ft_exit(data->rdl_args, data->envp, &(data->status), &(data->exit));
-  	free_all(cmds);
-	if (i > 9)
-		return (1);
-	return (0);
-}
-
-int	execute_command(char *path, t_data *data)
-{
-	int	child_pid = 0;
-	int	child_info = 0;
-
-	if (!(data->is_a_pipe))
-	{
-		if (sigaction(SIGINT, &(data->C_c_alt), NULL) == -1)
-			return (perror(""), errno);
-		child_pid = fork();
-	}
-	if (child_pid < 0)
-	{
-		perror("fork");
-		exit(errno);
-	}
-	if (!child_pid || data->is_a_pipe)
-	{
-		signal(SIGQUIT, SIG_DFL);
-		execve(path, data->rdl_args, data->envp);
-		perror("execve");
-		// free allocated memory
-		exit(errno);
-	}
-	else
-	{
-		if (!(data->is_a_pipe))
-		{
-			wait(&child_info);
-			if (sigaction(SIGINT, &(data->C_c), NULL) == -1)
-				return (perror(""), errno);	
-			if (f_sig == 2 && kill(0, SIGINT))
-				return (perror(""), errno);
-		}
-	}
-	if (WIFEXITED(child_info))
-		return (WEXITSTATUS(child_info));
-	else if (WIFSIGNALED(child_info))
-		return ((child_info & 127) + 128);
-		// return (WTERMSIG(status) + 128);
-	//if (!is_a_pipe && WIFEXITED(child_info))
-	//	return (WEXITSTATUS(child_info));
-	return (200);
-}
+// 	(void)av;
+// 	(void)ac;
+// 	sigemptyset(&(C_slash.sa_mask));
+//     sigemptyset(&(C_c.sa_mask));
+//     C_slash.sa_flags = 0;
+//     C_c.sa_flags = 0;
+//     C_slash.sa_handler = SIG_IGN;
+//     C_c.sa_handler = signal_handler;
+// 	envp = ft_duplicate(envp);
+//     term = ttyname(1);
+//     if (!term)
+//         {return (perror("minishell:"), free_all(envp), errno);}
+// 	while (1 && !s_exit)
+//     {
+//        // printf("term_path: %s\n", term);
+// 		sigaction(SIGQUIT, &C_slash, NULL);
+//         sigaction(SIGINT, &C_c, NULL);
+//         if (set_default_fd(term))
+//             return (free_all(envp), errno);
+// 		p = readline("minishell : ");
+// 		if (!p)
+//             exit_minishell(envp,p,0,"exit\n");
+//         if (p[0])
+//         {
+//             i = found_pipe(p,envp);
+//             if (i == 1)
+//             {
+//                 i = 0;
+//                 segments = c_split(p, '|',envp);
+//                 if (!segments)
+//                     return(exit_minishell(envp,p,1,"failed malloc\n"));//protect malloc                    
+//                 while(segments[i])
+//                 {
+//                     envp = parsing(&segments[i], envp, &s_exit);
+//                     i++;    
+//                 }
+//                 free_all(segments);
+//                 add_history(p);   
+//             }
+//             else if (i == 0)
+//             {
+//                 envp = parsing(&p, envp, &s_exit);
+//                 add_history(p);
+//             }
+//             else
+//                 return(exit_minishell(envp,p,1,"failed malloc\n"));//protect malloc
+//         }
+//         free(p);
+//     }
+//    	exit(s_exit); 
+// }
