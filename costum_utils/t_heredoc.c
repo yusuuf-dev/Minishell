800ab3d78de606af9193880238392fa468bd8849
yusuuf-dev <lucas2030garcia@gmail.com>
2025-07-29 12:29:15 +0100

    new update

--- costum_utils/t_heredoc.c
+++ costum_utils/t_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: yoel-you <yoel-you@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 10:41:26 by asoufian          #+#    #+#             */
-/*   Updated: 2025/07/26 09:44:12 by yoel-you         ###   ########.fr       */
+/*   Updated: 2025/07/29 12:12:29 by yoel-you         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,24 +121,24 @@ int	update_heredoc_lst(char *s, t_data *data, int found, int i)
 
 	q = 0;
 	temp = data->heredooc;
-	i = 0;
-	found = 0;
-	while ((s)[i] && !found)
+	while ((s)[i])
 	{
 		if (!q && (s[i] == '\'' || s[i] == '\"'))
 			q = s[i];
 		else if (q && s[i] == q)
 			q = 0;
 		if (!q && s[i] == '<' && s[i + 1] == '<')
-			found = 1;
+			found++;
 		i++;
 	}
 	if (!temp || !found)
 		return (1);
-	while (temp->next && temp->taken)
+	while (found)
 	{
-		temp = temp->next;
+		while (temp->next && temp->taken)
+			temp = temp->next;
+		temp->taken = 1;
+		found--;
 	}
-	temp->taken = 1;
 	return (1);
 }
